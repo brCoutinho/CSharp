afc6763f92c1e9954b5c561c445e6331b5fe43d3
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-02-06 19:44:35 -0300

    Condicionais

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 7 - Condicionais");
+
+            int idadeThiago = 13;
+            int quantidadePessoas = 2;
+
+            if (idadeThiago > 18)
+            {
+                Console.WriteLine("Maior de Idade, pode entrar!");
+            }
+            else
+            {
+                if (quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("Menor de Idade, porém acompanhado");
+                }
+                else
+                {
+                    Console.WriteLine("Menor de Idade, não pode entrar!");
+                }
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
