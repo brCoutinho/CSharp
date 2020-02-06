44bd931d276a101f2d34f881363ca138e2046c84
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-02-06 14:54:49 -0300

    Usando Pontos Flutuantes

---
+++ 3-CriandoVariaveisFloat/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CriandoVariaveisFloat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 3 - Criando Variáveis Float");
+
+            double salario;
+
+            salario = 3000.00;
+            double numeroaaleatorio;
+            numeroaaleatorio = 15.0 / 2;
+
+            Console.WriteLine(numeroaaleatorio);
+
+            Console.WriteLine("Seu salário é " + salario);
+
+            Console.WriteLine("Execução Finalizada. Tecle Enter para sair");
+
+            Console.ReadLine();
+        }
+    }
+}
