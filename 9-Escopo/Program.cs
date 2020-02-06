e7a5309a0cc8cff7fe0e71e9ebbb20173c00350b
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-02-06 20:50:53 -0300

    Escopo

---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 9 - Escopo");
+
+            int idadeThiago = 23;
+            int quantidadePessoas = 2;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "Está acompanhado";
+            }
+            else
+            {
+                mensagemAdicional = "Está desacompanhado";
+            }
+
+            if (idadeThiago > 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar!");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
