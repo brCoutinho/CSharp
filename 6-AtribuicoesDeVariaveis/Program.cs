1e2f0fbb35ee01b6ff84240cd105f6776c04c9d0
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-02-06 17:47:44 -0300

    Atribuĩções de Variáveis

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 23;
+            int idadeThiago = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idadeThiago);
+
+
+            Console.ReadLine();
+        }
+    }
+}
