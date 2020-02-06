78f0edf7d4ffa65660e9bf0d3d3dd105e71f0e2d
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-02-06 20:49:48 -0300

    Condicionais

---
+++ 8-CondicionaisSegundaParte/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_CondicionaisSegundaParte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 8 - Condicionais");
+
+            int idadeThiago = 23;
+            int quantidadePessoas = 2;
+            bool acompanhado = quantidadePessoas >= 2;
+
+            if (idadeThiago > 18 && acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
