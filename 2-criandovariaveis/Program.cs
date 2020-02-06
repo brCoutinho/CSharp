93b035da06948e21013dccc9fedd228c4ced87d5
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-02-06 14:51:24 -0300

    Criando Variáveis

---
+++ 2-criandovariaveis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 2 - Criando Variáveis");
+
+            int idade;
+            idade = 23;
+
+            Console.WriteLine("Sua idade é "+ idade);
+
+            idade += idade;
+
+            Console.WriteLine(idade);
+
+
+
+            Console.WriteLine("Execução Finalizada. Tecle Enter para sair");
+
+            Console.ReadLine();
+        }
+    }
+}
