f387dcdc9bfa9664469196223e147f1d29d658de
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-02-06 16:01:33 -0300

    Caracteres

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char primeiraLetra = 'A';
+
+            primeiraLetra = (char)65;
+
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Thiago Coutinho, Dev " + 1997;
+            Console.WriteLine(titulo);
+
+            string linguagensEstudadas =
+ @"- .NET
+- Java
+- Javascript";
+
+            Console.WriteLine(linguagensEstudadas);
+
+            Console.ReadLine();
+        }
+    }
+}
