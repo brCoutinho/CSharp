eea71e874ea1df7ff9c26a723bfc99c290f78513
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-02-06 20:52:19 -0300

    Usando o For

---
+++ a11-CalculaPoupancaFor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a11_CalculaPoupancaFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 11 - Calcula Poupança");
+
+            double valorInvestido = 1000;
+             
+            for(int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido *= 1.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$ " + valorInvestido);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
